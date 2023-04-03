30bfc329f032c7f8e90cfbd206c7676c0beea0c7
lu-carvalho <lucarvalhonj@gmail.com>
2023-04-03 20:39:59 -0300

    refactor: added product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+  NotFoundException,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { FilterProductDto } from './dto/filter-product.dto';
 
-@Controller('product')
+@Controller('search/products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @Get('/')
+  async getProducts(@Query() filterProductDTO: FilterProductDto) {
+    // apply filter if any, otherwise return all products
+    if (Object.keys(filterProductDTO).length) {
+      const foundProductsByFilter =
+        await this.productService.findProductsByFilter(filterProductDTO);
+      return foundProductsByFilter;
+    } else {
+      const allProducts = await this.productService.findAllProducts();
+      return allProducts;
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.productService.findAll();
+  @Post('/')
+  async addNewProduct(@Body() createProductDto: CreateProductDto) {
+    const newProduct = await this.productService.createProduct(
+      createProductDto,
+    );
+    return newProduct;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  @Get('/:id')
+  async getProductById(@Param('id') id: string) {
+    const foundProduct = await this.productService.findProductById(id);
+    if (!foundProduct)
+      throw new NotFoundException(
+        'Are you mistaking us for someone else?! Sorry, we do not have that product..',
+      );
+    return foundProduct;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  @Put('/:id')
+  async updateProduct(@Param('id') id: string) {
+    const productToUpdate = await this.productService.updateProduct(
+      id,
+      UpdateProductDto,
+    );
+    if (!productToUpdate)
+      throw new NotFoundException(
+        'Are you mistaking us for someone else?! Sorry, we do not have that product..',
+      );
+    return productToUpdate;
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
+    const productToDelete = await this.productService.deleteProductById(id);
+    if (!productToDelete)
+      throw new NotFoundException(
+        'You can not delete what already does not exist... Sorry, we do not have that product',
+      );
+    return productToDelete;
   }
 }
