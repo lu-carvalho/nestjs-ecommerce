33fa7d1f6635c4b61e9b07687b52d32204eafe8c
lu-carvalho <lucarvalhonj@gmail.com>
2023-04-11 21:19:53 -0300

    feat: finalizing auth implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { Roles } from './decorators/roles.decorator';
+import { Role } from './enums/role.enum';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { LocalAuthGuard } from './guards/local.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @Post('/register')
+  async registerNewUser(@Body() createUserDto: CreateUserDto) {
+    const newUser = await this.userService.createUser(createUserDto);
+    return newUser;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async logInUser(@Request() req) {
+    return this.authService.logUserAsJWT(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
+  @Get('/user')
+  getLoggedUserProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('/admin')
+  getAdminDashboard(@Request() req) {
+    return req.user;
+  }
+}
